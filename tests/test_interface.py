3b538e9e4c79eabed73b46da6575edc4704220fe
Max <maxs987@gmail.com>
2021-08-08 21:39:51 +0300

    added another test

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -8,3 +8,8 @@ def test_glob(data_root):
 
     assert set(repo.glob('images/*.png', version=Local)) == set(
         map(Path, ['images/1.png', 'images/2.png', 'images/3.png']))
+
+
+def test_from_here():
+    repo = Repository.from_here('data')
+    assert repo.root.resolve() == Path('data').resolve()
