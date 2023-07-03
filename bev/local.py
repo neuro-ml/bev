99e735a69764c80db9dd3876d0f3fac54c8a1dbb
Max <max@ira-labs.com>
2023-07-03 01:13:17 +0100

    pickleable `Local`. closes #28

--- bev/local.py
+++ bev/local.py
@@ -1,8 +1,9 @@
 class LocalVersion:
     """A class used to denote the current uncommitted version of data"""
 
-    def __getstate__(self):  # pragma: no cover
-        raise RuntimeError('This object cannot be pickled')
+    @classmethod
+    def __getversion__(cls):
+        raise RuntimeError("The `Local` object doesn't have a well-defined version")
 
     def __eq__(self, other):
         return isinstance(other, LocalVersion)
